35efcf9bb5c232153c0f266aa8be4b939a54b2a4
davo999666 <davoaleks1991@gmail.com>
2026-02-01 20:04:06 +0200

    four

--- src/utils/classNameSize.ts
+++ src/utils/classNameSize.ts
@@ -1,12 +1,12 @@
 
 export const pieceSize = `
   w-[5.2vw] h-[5vw]             /* default small screens */
-  sm:w-[5.2vw] sm:h-[4.8vw]         /* small phones */
-  md:w-[5.2vw] md:h-[4.8vw]         /* tablets */
-  lg:w-[5.2vw] lg:h-[4.8vw]         /* laptops */
-  xl:w-[5.2vw] xl:h-[4.8vw]     /* desktops */
-  2xl:w-[5.2vw] 2xl:h-[4.8vw]       /* very large screens */
+  sm:w-[5.5vw] sm:h-[5vw]         /* small phones */
+  md:w-[5.5vw] md:h-[5vw]         /* tablets */
+  lg:w-[5.5vw] lg:h-[5vw]         /* laptops */
+  xl:w-[5.5vw] xl:h-[5vw]     /* desktops */
+  2xl:w-[5.5vw] 2xl:h-[5vw]       /* very large screens */
   max-w-[130px] max-h-[120px]   /* cap size on big screens */
   select-none cursor-grab active:cursor-grabbing
 `;
-export const boardSize =`w-[50vw] h-[40vw]`
+export const boardSize =`w-[50vw] h-[45vw]`
