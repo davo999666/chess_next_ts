7079e82c2711c2b639b4d1840945de765d1c0735
davo999666 <davoaleks1991@gmail.com>
2026-02-02 19:14:02 +0200

    four

--- src/utils/classNameSize.ts
+++ src/utils/classNameSize.ts
@@ -4,8 +4,8 @@ export const pieceSize = `
   sm:w-[7vw] sm:h-[7vw]         /* small phones */
   md:w-[7vw] md:h-[7vw]         /* tablets */
   lg:w-[6vw] lg:h-[5.5vw]         /* laptops */
-  xl:w-[5.5vw] xl:h-[5vw]     /* desktops */
-  2xl:w-[5.5vw] 2xl:h-[5vw]       /* very large screens */
+  xl:w-[5vw] xl:h-[5vw]     /* desktops */
+  2xl:w-[5vw] 2xl:h-[5vw]       /* very large screens */
   max-w-[130px] max-h-[120px]   /* cap size on big screens */
   select-none cursor-grab active:cursor-grabbing
 `;
@@ -13,9 +13,9 @@ export const boardSize = `
   w-[67vw] h-[75vw]           /* default small screens: fills most width */
   sm:w-[67vw] sm:h-[75vw]     /* small phones */
   md:w-[67vw] md:h-[75vw]     /* tablets */
-  lg:w-[45vw] lg:h-[40vw]     /* laptops */
-  xl:w-[45vw] xl:h-[40vw]     /* desktops */
-  2xl:w-[45vw] 2xl:h-[40vw]   /* very large screens */
+  lg:w-[40vw] lg:h-[40vw]     /* laptops */
+  xl:w-[40vw] xl:h-[40vw]     /* desktops */
+  2xl:w-[40vw] 2xl:h-[40vw]   /* very large screens */
   max-w-[960px] max-h-[960px] /* never exceed 960x960 */
   shadow-2xl rounded-md
 `;
